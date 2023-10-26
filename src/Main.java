6b0ce1c5c16216cd1addbc1475fb6c0f91005691
Astrid Rosen <astrid.flisan@gmail.com>
2023-10-26 13:48:43 +0200

    Astrid branch

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        String test = "Lises branch";
+        String test = "Astrids branch";
     }
 }
