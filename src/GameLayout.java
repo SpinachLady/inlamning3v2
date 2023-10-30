952e9aab0ddd1a802d12668647356fb297faa186
Astrid Rosen <astrid.flisan@gmail.com>
2023-10-30 12:37:23 +0100

    -

---
+++ src/GameLayout.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class GameLayout {
+
+    private final Dimension numberButtonSize = new Dimension(50, 50);
+    private final Dimension labelSize = new Dimension(250, 40);
+    private final Color color1 = new Color(164, 201, 255);
+    private final Color color2 = new Color (164, 255, 185);
+    private final Font numberButtonFont = new Font("Roboto", Font.BOLD, 25);
+    private final Font regularFont = new Font("Roboto", Font.PLAIN, 16);
+    private final Border thinBorder = BorderFactory.createLineBorder(Color.black);
+
+    public Dimension getNumberButtonSize () {
+        return numberButtonSize;
+    }
+    public Dimension getLabelSize () {
+        return labelSize;
+    }
+    public void setTopLabelLayout (JLabel label) {
+        label.setOpaque(true);
+        label.setBackground(color1);
+        label.setPreferredSize(labelSize);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setFont(regularFont);
+        label.setBorder(thinBorder);
+    }
+    public void setNumberButtonLayout (JButton button) {
+        button.setPreferredSize(numberButtonSize);
+        button.setBackground(color1);
+        button.setFont(numberButtonFont);
+        button.setMargin(new Insets(2, 2, 2, 2));
+        button.setBorder(thinBorder);
+    }
+    public Color getColor2 () {
+        return color2;
+    }
+    public void setShuffleButtonLayout (JButton button) {
+        button.setBackground(color1);
+        button.setFont(regularFont);
+        button.setBorder(thinBorder);
+    }
+}
